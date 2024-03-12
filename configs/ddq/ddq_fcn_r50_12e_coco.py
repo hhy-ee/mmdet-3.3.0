db594c80291bb5162da774c2c0837471637ff58b
root <root@email.com>
2024-03-12 23:15:01 +0000

    1

--- configs/ddq/ddq_fcn_r50_12e_coco.py
+++ configs/ddq/ddq_fcn_r50_12e_coco.py
@@ -89,7 +89,7 @@ train_dataloader = dict(
 # optimizer
 optim_wrapper = dict(
     type='OptimWrapper',
-    optimizer=dict(type='AdamW', lr=1e-4 * 0.5, weight_decay=0.05),
+    optimizer=dict(type='AdamW', lr=2e-4 * 0.5, weight_decay=0.05),
     clip_grad=dict(max_norm=0.1, norm_type=2),
     paramwise_cfg=dict(custom_keys={'backbone': dict(lr_mult=0.1)}))
 
